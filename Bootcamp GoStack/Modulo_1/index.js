94137be122fd53a6e65597e867978909a98ec462
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2020-03-16 17:46:25 -0300

    Add files via upload

--- Bootcamp GoStack/Modulo_1/index.js
+++ Bootcamp GoStack/Modulo_1/index.js
@@ -11,7 +11,47 @@ server.use(express.json());
 
 const users = ["Joao", "Victor", "Ayalla"];
 
-server.get("/teste", (req, res) => {
+server.use((req, res, next) => {
+  console.log("A requisição foi chamada!");
+
+  // retorno next para que o resto da aplicacao nao seja interrompido
+  return next();
+});
+
+server.use((req, res, next) => {
+  console.time("Request");
+  console.log(`Método: ${req.method}; URL: ${req.url}`);
+
+  // chamo next para executar o que tem abaixo no codigo
+  next();
+
+  // finalizo com o log do tempo de execução
+  console.timeEnd("Request");
+});
+
+// verificar se no corpo da requisição existe o user
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "User name is required" });
+  }
+
+  return next();
+}
+
+// verificar se o indice que quer ser acessado existe
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+
+  if (!user) {
+    return res.status(400).json({ error: "User does not exists" });
+  }
+
+  req.user = user;
+
+  return next();
+}
+
+server.get("/teste", checkUserInArray, (req, res) => {
   const nome = req.query.nome;
 
   // para o query
@@ -20,7 +60,7 @@ server.get("/teste", (req, res) => {
   });
 });
 
-server.get("/users1/:id", (req, res) => {
+server.get("/users1/:id", checkUserInArray, (req, res) => {
   const id = req.params.id;
 
   // para o route com o id
@@ -29,18 +69,18 @@ server.get("/users1/:id", (req, res) => {
   });
 });
 
-server.get("/users2/:index", (req, res) => {
+server.get("/users2/:index", checkUserInArray, (req, res) => {
   const { index } = req.params;
 
   // para o route retornar o user com aquele index
-  return res.json(users[index]);
+  return res.json(req.user); // posso retornar req.user vindo da funcao checkUserInArray
 });
 
-server.get("/users", (req, res) => {
+server.get("/users", checkUserInArray, (req, res) => {
   return res.json(users); // retorna todos os usuarios
 });
 
-server.post("/users", (req, res) => {
+server.post("/users", checkUserExists, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -49,7 +89,7 @@ server.post("/users", (req, res) => {
   return res.json(users);
 });
 
-server.put("/users/:index", (req, res) => {
+server.put("/users/:index", checkUserExists, checkUserInArray, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -59,7 +99,7 @@ server.put("/users/:index", (req, res) => {
   return res.json(users);
 });
 
-server.delete("/users/:index", (req, res) => {
+server.delete("/users/:index", checkUserInArray, (req, res) => {
   const { index } = req.params;
 
   users.splice(index, 1);
